5ebd57b5f29725fd917c9724e705bb8bd10891a6
Fabiano Ventura Guimarães <fabianovguimaraes@hotmail.com>
2022-08-15 18:17:37 -0300

    Tipos_primitivos_15.08

---
+++ ej/TiposPrimitivos/ExemploInt.java
@@ -0,0 +1,10 @@
+public class ExemploInt {
+
+    public static void main(String[] args) {
+      int populacaoUberlandia = 699_097; // depois do java 7,. podemos usar o underline eentre as casas dos milhares. 
+  
+      // int populacaoUberlandia2 = 2_147_483_648; // não compila
+      int populacaoUberlandia2 = 2_147_483_647;
+    }
+  
+  }
