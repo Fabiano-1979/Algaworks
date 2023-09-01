6547507410b667acb209d5fd855a9971ccf3b013
Fabiano-1979 <fabianovguimaraes@hotmail.com>
2023-08-31 21:29:30 -0300

    Atu1

---
+++ ej/2_Fundamentos_liguagem_java/Exercicios/DesafioPromocaoAritmetica.java
@@ -0,0 +1,12 @@
+public class DesafioPromocaoAritmetica {
+    public static void main(String[] args) {
+        int idade1 = 30;
+        int idade2 = 40;
+        int idade3 = 60;
+    
+        Float idadeMedia = (idade1 + idade2 + idade3) / 3f; // Aqui, o resultado , foi alcansado simplimente  
+        // trocando o tipo da variável idadeMedia e, ao dividir por 3, acrescentar um "f" no número 3
+    
+        System.out.println("Idade média: " + idadeMedia);
+      }
+}
