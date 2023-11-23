7e85ff74b8345981e459cb5e4f27bded6a4981b0
Fabiano-1979 <fabianovguimaraes@hotmail.com>
2023-11-23 17:32:02 -0300

    Atualiza

---
+++ ej/2_Fundamentos_liguagem_java/ComentarioEmCodigo/ComentarioEmCodigo.java
@@ -0,0 +1,20 @@
+// exemplo de comentario 
+
+public class OlaMergulhador {
+
+  public static void main(String[] args) { // pode ser em qualquer lugar 
+    // o cometario
+    // Temos tambem o comentario de BLOCO (  /* pULANDO LINHAS */) ex: 
+
+    /*
+      Esse e um comentario de BLOCO
+      Sendo posivel pular linha. 
+     
+     */
+
+
+    System.out.println("Ola, mergulhador: Fabiano!");
+  }
+
+
+}
