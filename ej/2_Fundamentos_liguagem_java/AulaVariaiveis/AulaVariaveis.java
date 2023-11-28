f7d936a20a67556d9e8d80ce9ae0b87e060a3f69
Fabiano-1979 <fabianovguimaraes@hotmail.com>
2023-11-28 17:17:33 -0300

    atualização

--- ej/2_Fundamentos_liguagem_java/AulaVariaiveis/AulaVariaveis.java
+++ ej/2_Fundamentos_liguagem_java/AulaVariaiveis/AulaVariaveis.java
@@ -5,9 +5,9 @@ public class AulaVariaveis {
   public static void main(String[] args) {
   
   int minhaIdade,suaIdade,soma;
-  minhaIdade = 42; 
-  suaIdade = 25;
-  soma = minhaIdade+suaIdade;
+    minhaIdade = 42; 
+    suaIdade = 25;
+    soma = minhaIdade+suaIdade;
   // ou pode ser atribuido em um unica linha, como exemplo: int minhaIdade = 42; 
   
   System.out.println("Minha idade é : "+minhaIdade+" Sua idade é : "+suaIdade+" A soma das DUAS IDADES É : "+soma);
